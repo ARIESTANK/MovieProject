0d331622635c89ece4643bffc061d7228ab7d3a8
ARIESTANK <kaungkhantlin2332003@gmail.com>
2023-10-14 22:13:04 +0630

    second commit

--- Home.js
+++ Home.js
@@ -1,41 +1,19 @@
-const gallery = document.querySelector('.trilogy');
-const imageWrappers = document.querySelectorAll('.image-wrapper');
-let currentIndex = 1;
+rtl: false
 
-function moveToIndex(index) {
-  const offset = -index * 100;
-  gallery.style.transform = `translateX(${offset}%)`;
-}
-
-function swipeLeft() {
-  currentIndex = (currentIndex - 1 + imageWrappers.length) % imageWrappers.length;
-  moveToIndex(currentIndex);
-}
-
-function swipeRight() {
-  currentIndex = (currentIndex + 1) % imageWrappers.length;
-  moveToIndex(currentIndex);
-}
-
-setInterval(swipeRight, 3000); // Automatically swipe every 3 seconds (adjust as needed)
-
-// Add touch event listeners for swiping on mobile devices
-let startX = null;
-gallery.addEventListener('touchstart', (e) => {
-  startX = e.touches[0].clientX;
-});
-
-gallery.addEventListener('touchend', (e) => {
-  if (startX) {
-    const endX = e.changedTouches[0].clientX;
-    const deltaX = startX - endX;
-
-    if (deltaX > 50) {
-      swipeRight();
-    } else if (deltaX < -50) {
-      swipeLeft();
-    }
-
-    startX = null;
+$('.owl-carousel').owlCarousel({
+  rtl:true,
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
   }
-});
+})
