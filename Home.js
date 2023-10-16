65e326717479a690d9ed4fadca54f6cce98fde22
ARIESTANK <kaungkhantlin2332003@gmail.com>
2023-10-16 20:51:33 +0630

    first commit

--- Home.js
+++ Home.js
@@ -1,19 +1,20 @@
-rtl: false
 
-$('.owl-carousel').owlCarousel({
-  rtl:true,
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
+document.addEventListener('DOMContentLoaded',()=>{
+    let menu=document.getElementById('burger');
+    menu.addEventListener('click',()=>{
+        let burger=document.getElementById('menu');
+            burger.style.display="block";
+            menu.style.display="none";
+       
+    })
+})
+document.addEventListener('DOMContentLoaded',()=>{
+    let menu1=document.getElementById('menu1');
+    menu1.addEventListener('click',()=>{
+        let menu=document.getElementById('menu');
+        let burger=document.getElementById('burger');
+            burger.style.display="block";
+            menu.style.display="none";
+       
+    })
 })
